81e714b21fa27f57a0654281d7c5dbb21293a31a
pp2_21b030707 <a_orynbai@kbtu.kz>
2022-03-31 00:28:47 +0600

    seventh commit changes

--- week7/1.py
+++ week7/1.py
@@ -1,29 +1,97 @@
-'''
-(only with minutes and seconds) 
-which is synchronized with system clock.
-Use Mickey's right hand as minutes arrow 
-and left - as seconds.
-'''
-import pygame
-mainclock=pygame.time.Clock()
-from pygame.locals import *
-pygame.init()
-screen=pygame.display.set_mode((800,598))
-img=pygame.image.load('clock.png')
-img2=pygame.image.load('sec.png')
+import pygame, time
 
-angle=0
-centerx=400
-centery=598/2
-running=True
-while running:
-    angle+=1
-    screen.blit(img,(0,0))
-    mx,my=pygame.mouse.get_pos()
-    img2_copy=pygame.transform.rotate(img2,angle)
-    screen.blit(img2_copy,(centerx-140,centery-300))
+
+#defining main properties for arrows
+class Arrows(pygame.sprite.Sprite):
+    #initializing arrow
+    def __init__(self, pos, img, freq):
+        super().__init__()
+        self.img, self.image = img, img
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+        self.last_rotation = pygame.time.get_ticks()
+        self.freq, self.angle, self.rev = freq, 0, 0
+        all_sprites.add(self)
+    
+    #rotating arrows each second(here it is only used with second's arrow)
+    def update(self):
+        now = pygame.time.get_ticks()
+        if (now - self.last_rotation) // 1000 >= self.freq:
+            self.last_rotation = now 
+            new_image = pygame.transform.rotate(self.img, self.angle)
+            if self.angle == 0:
+                self.rev += 1
+            self.angle = (self.angle - 6) % 360
+            x, y = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = (x, y)
+
+
+#updated arrow class for minute arrow
+class MinuteArrow(Arrows):
+    def __init__(self, pos, img, freq):
+        super().__init__(pos, img, freq)
+    
+    #minute arrow rotates only when second arrow is on the 12
+    def update(self):
+        if self.rev < seconds.rev:
+            new_image = pygame.transform.rotate(self.img, self.angle)
+            self.angle = (self.angle - 6) % 360
+            x, y = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = (x, y)
+            self.rev += 1
+
+
+#loading images and turning them on vertical position
+second_hand = pygame.image.load('sec.png')
+second_hand = pygame.transform.rotate(second_hand, 60)
+minute_hand = pygame.image.load('min.png')
+minute_hand = pygame.transform.rotate(minute_hand, -48)
+background = pygame.image.load('clock.png')
+
+
+#initializing main settings
+pygame.init()
+screen = pygame.display.set_mode((1300, 960))
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+app = True
+
+
+#creating second and minute arrows
+minutes = MinuteArrow((650, 520), minute_hand, 60)
+seconds = Arrows((660, 510), second_hand, 1)
+
+
+#getting initial time in minutes and seconds
+now = int(time.time())
+start_minutes = ((now) // 60) % 60
+start_seconds = (now ) % 60
+
+
+#setting arrows in the right initial position
+seconds.angle = -6 * start_seconds
+minutes.angle = -6 * start_minutes
+seconds.rev += 1
+
+
+#main loop
+while app:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running=False
+            app = False
+    
+    screen.fill('WHITE')
+    screen.blit(background, (-40, -20))
+    
+    
+    all_sprites.update()
+    all_sprites.draw(screen)
+    
+    
+    pygame.display.flip()
     pygame.display.update()
-    mainclock.tick(60)
+pygame.quit()
