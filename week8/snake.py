10908249215934449edf905d1846433e4c51afcf
pp2_21b030707 <a_orynbai@kbtu.kz>
2022-04-10 21:00:00 +0600

    for review

--- week8/snake.py
+++ week8/snake.py
@@ -1,24 +1,26 @@
-#ADDING WALLS - no collision check
+#collision check with the walls
 import pygame
 import random
 green=(0,100,0)
 black=(0,0,0)
 red=(255,0,0)
 grey=(69,69,69)
+yellow=(250,253,15)
 pygame.init()
 window_width,window_height=500,500
 screen=pygame.display.set_mode((window_width,window_height))
 class Snake:
     def __init__(self, x, y):
         self.size = 1
-        self.elements = [[x, y]]  # [[x0, y0], [x1, y1], [x2, y2] ...] (i) -> (i - 1)
+        self.elements = [[x, y]]  
         self.radius = 10
-        self.dx = 5  # Right.
+        self.dx = 5 
         self.dy = 0
         self.add_size = False
         self.speed = 25
         self.score=0
         self.level=1
+        self.running=True
     def draw(self):
         for element in self.elements:
             pygame.draw.circle(screen, (black), element, self.radius)
@@ -41,25 +43,32 @@ class Snake:
             self.elements[i][0] = self.elements[i - 1][0]
             self.elements[i][1] = self.elements[i - 1][1]
         self.elements[0][0] += self.dx
-        self.elements[0][1] += self.dy
-        if self.elements[0][0] > window_width:
-            self.elements[0][0]=0
-        elif self.elements[0][0]<0:
+        self.elements[0][1]+= self.dy
+        if self.elements[0][0] > window_width: 
+            self.elements[0][0]=0 
+        elif self.elements[0][0]<0: 
             self.elements[0][0]=window_width
-        if self.elements[0][1] >window_height:
+        if self.elements[0][1] >window_height: 
             self.elements[0][1] =0
-        elif self.elements[0][1]<0:
+        elif self.elements[0][1]<0: 
             self.elements[0][1]=window_height
+    def collision_check(self):
+        if snake.level>=2:
+            if self.elements[0][0]>=300 and self.elements[0][0]<=340 and self.elements[0][1]>=0 and self.elements[0][1]<=150:
+                self.running=False
+            elif self.elements[0][0]>=150 and self.elements[0][0]<=200 and self.elements[0][1]>=300 and self.elements[0][1]<=window_height:
+                self.running=False
+            return self.running
+        else:
+            return self.running
     def eat(self, foodx1, foody1):
-        x = self.elements[0][0]
-        y = self.elements[0][1]
-        if foodx1 <= x <= foodx1 + 10 and foody1 <= y <= foody1 + 10:
+        if foodx1 <= self.elements[0][0] <= foodx1 + 10 and foody1 <= self.elements[0][1] <= foody1 + 10:
             self.score+=1
             if self.score % 3 == 0:
                 self.speed += 5
                 self.level+=1
             return True
-        return False
+        return False  
     def walls(self):
         if self.level>=2:
             return True
@@ -69,25 +78,23 @@ class Food:
         self.y = random.randint(1, window_height)
 
     def gen(self):
+        self.x = random.randint(1, window_width)
+        self.y = random.randint(1, window_height)
         if snake.level>=2:
-            for i in range(3):
-                prex = random.choice([(1,150),(200,300),(341,500)])
-                self.x=(random.randint(*prex))           
-            self.y=random.randint(151,300)
-        else:
-            self.x = random.randint(1, window_width)
-            self.y = random.randint(1, window_height)
+            if self.x>=300 and self.x<=340:
+                self.y=random.randint(151,window_height)
+            elif self.x>=150 and self.x<=200:
+                self.y=random.randint(1,300)
     def draw(self):
         pygame.draw.rect(screen, red, (self.x, self.y, 10, 10))
+
 snake = Snake(100, 100) #100,100 is the initial position
 food1 = Food()
-running = True
 FPS = 30
 d = 5
 clock = pygame.time.Clock()
-while running:
+while snake.collision_check():
     clock.tick(snake.speed)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
